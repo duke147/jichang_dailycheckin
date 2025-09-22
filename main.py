cba133f294ea9edbc0839ac99dbeab57510b26d8
wangfugui <85614628@qq.com>
2025-09-22 14:58:53 +0800

    feat: new server

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ SCKEY = os.environ.get('SCKEY')
 Token = os.environ.get('TOKEN')
 def push(content):
     if SCKEY != '1':
-        url = "https://sctapi.ftqq.com/{}.send?title={}&desp={}".format(SCKEY, 'ikuuu签到', content)
+        url = "https://11566.push.ft07.com/send/{}.send?title={}&desp={}".format(SCKEY, 'ikuuu签到', content)
         requests.post(url)
         print('推送完成')
     elif Token != '1':
